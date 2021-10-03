f69369035e588ea4655b6d419492e76717642681
Reka Madarasz <mreka91@gmail.com>
2021-10-03 16:50:27 +0200

    alumni

---
+++ wp-content/themes/rudolfsteiner/blocks/alumni/block.php
@@ -0,0 +1,5 @@
+<div>
+    <img class="alumni-img" src="<?php block_field("alumni-image"); ?>" />
+    <h3><?php block_field("alumni-quote"); ?></h3>
+    <p><?php block_field("alumni-name"); ?></p>
+</div>
