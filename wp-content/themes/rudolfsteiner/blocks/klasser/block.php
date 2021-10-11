1f903693b40e02aac282fe202777274b81abfe83
Reka Madarasz <mreka91@gmail.com>
2021-10-11 10:50:26 +0200

    Klasser on mobile and desktop

---
+++ wp-content/themes/rudolfsteiner/blocks/klasser/block.php
@@ -0,0 +1,5 @@
+<div class="klasser-container">
+    <h2><?php block_field("klasser-title"); ?></h2>
+    <p><?php block_field("klasser-text"); ?></p>
+    <img class="klasser-img" src="<?php block_field("klasser-image"); ?>" />
+</div>
